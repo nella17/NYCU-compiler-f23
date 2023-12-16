99310b91785892ade0d18d15da3500473e1fc897
Allen Guan <me@nella17.tw>
2023-12-16 22:49:24 +0800

    chore: visitChildNodes

--- src/lib/sema/SemanticAnalyzer.cpp
+++ src/lib/sema/SemanticAnalyzer.cpp
@@ -59,16 +59,7 @@ void SemanticAnalyzer::visit(VariableNode &p_variable) {
 }
 
 void SemanticAnalyzer::visit(ConstantValueNode &p_constant_value) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    // TODO
 }
 
 void SemanticAnalyzer::visit(FunctionNode &p_function) {
@@ -107,120 +98,48 @@ void SemanticAnalyzer::visit(CompoundStatementNode &p_compound_statement) {
 }
 
 void SemanticAnalyzer::visit(PrintNode &p_print) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_print.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(BinaryOperatorNode &p_bin_op) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_bin_op.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(UnaryOperatorNode &p_un_op) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_un_op.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(FunctionInvocationNode &p_func_invocation) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_func_invocation.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(VariableReferenceNode &p_variable_ref) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_variable_ref.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(AssignmentNode &p_assignment) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_assignment.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(ReadNode &p_read) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_read.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(IfNode &p_if) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_if.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(WhileNode &p_while) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_while.visitChildNodes(*this);
+    // TODO
 }
 
 void SemanticAnalyzer::visit(ForNode &p_for) {
@@ -234,14 +153,6 @@ void SemanticAnalyzer::visit(ForNode &p_for) {
 }
 
 void SemanticAnalyzer::visit(ReturnNode &p_return) {
-    /*
-     * TODO:
-     *
-     * 1. Push a new symbol table if this node forms a scope.
-     * 2. Insert the symbol into current symbol table if this node is related to
-     *    declaration (ProgramNode, VariableNode, FunctionNode).
-     * 3. Travere child nodes of this node.
-     * 4. Perform semantic analyses of this node.
-     * 5. Pop the symbol table pushed at the 1st step.
-     */
+    p_return.visitChildNodes(*this);
+    // TODO
 }
